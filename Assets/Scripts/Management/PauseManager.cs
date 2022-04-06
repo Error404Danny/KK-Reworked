b4c2fa86c29ae672f72176856d420c93cfec81b7
Error404Danny <danny.lopez@loop.colum.edu>
2022-04-06 18:27:03 -0500

    Copied Old project to a new folder
    Used the same files from Kitten Klash and duplicated all aspects of the project. A new folder was made for it as well as a new repo

---
+++ Assets/Scripts/Management/PauseManager.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject GamePanel;
+    public GameObject PausedPanel;
+    public GameObject BoardPanel;
+    public GameObject MenuButtonBackgroud;
+    public GameObject Timed;
+    private GameObject board;
+    public bool isPaused = false;
+
+
+    public GameObject GameOverText;
+    public GameObject PauseText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        Timed.SetActive(true);
+        board = GameObject.FindGameObjectWithTag("Board");
+    }
+
+    public void QuitGame()
+    {
+        //Closes Application
+        Application.Quit();
+    }
+    public void PauseGame()
+    {
+        //Pauses Game when the button is hit. 
+        //Also stops the time from the timer. 
+        if (isPaused)
+        {
+            
+            Time.timeScale = 0;
+            board.SetActive(false);
+            isPaused = false;
+            BoardPanel.SetActive(true);
+            MenuButtonBackgroud.SetActive(true);
+            Timed.SetActive(true);
+        }
+        else
+        {
+            board.SetActive(true);
+            Time.timeScale = 1;
+            isPaused = true;
+            Timed.SetActive(false);
+            MenuButtonBackgroud.SetActive(false);
+            BoardPanel.SetActive(false);
+            PausedPanel.SetActive(true);
+        }
+    }
+    public void UnPauseGame()
+    {
+        //IF TAKEN OUT... Button does not work properly
+        //When hit in Pause Menu, starts to countdown time and game goes back to normal.
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+            BoardPanel.SetActive(true);
+            PausedPanel.SetActive(false);
+            MenuButtonBackgroud.SetActive(true);
+            Timed.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+            MenuButtonBackgroud.SetActive(false);
+            BoardPanel.SetActive(false);
+            //PausedPanel.SetActive(true);
+            Timed.SetActive(false);
+        }
+    }
+    public void ReloadScene()
+    {
+        //Reloads the game. 
+        SceneManager.LoadScene("djscene", LoadSceneMode.Single);
+        //Fixes Issue with Timer.
+        //Reloads Timer and then counts down when the game is restarted
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+            PausedPanel.SetActive(false);
+            Timed.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+            Timed.SetActive(false);
+            PausedPanel.SetActive(true);
+        }
+    }
+    public void ReturnMenu()
+    {
+        //Code I have put does not work... :(
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                UnPauseGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void GameOver()
+    {
+        board.SetActive(true);
+        Time.timeScale = 1;
+        isPaused = true;
+        Timed.SetActive(false);
+        MenuButtonBackgroud.SetActive(false);
+        BoardPanel.SetActive(false);
+        PausedPanel.SetActive(true);
+        GameOverText.SetActive(true);
+        PauseText.SetActive(false);
+
+    }
+}
---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public GameObject MenuPanel;
+    public GameObject CreditsPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Starts in Menu
+        //Hides the Credits
+        MenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+
+    }
+     public void ShowCredits()
+    {
+        //Shows Credits when not in Menu
+        //Hides Menu and the buttons 
+        MenuPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+    }
+    public void ShowMenuPanel()
+    {
+        //Shows Menu Options when not in Credits
+        //Hides Credits Menu
+        MenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+    }
+    public void PlayGame()
+    {
+        //Plays the game...
+        //Switch out the "SampleScene" with the game scene 
+        StartCoroutine(DeepMeowWait());
+        //SceneManager.LoadScene("djscene", LoadSceneMode.Single);
+    }
+    public void QuitGame()
+    {
+        //Closes Application
+        Application.Quit();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator DeepMeowWait()
+    {
+        FindObjectOfType<AudioManager>().Play("DeepMeow");
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene("djscene", LoadSceneMode.Single);
+
+
+
+    }
+}
